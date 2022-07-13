b0fb73a9239954cb1e36c7182090a0c4d4aad29e
Stephanie Chan <103970034+eina-chan@users.noreply.github.com>
2022-07-13 17:32:56 +1000

    Exercise 6 - extra credit 1 corrected

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -2,66 +2,36 @@
 // http://localhost:3000/isolated/exercise/06.js
 
 import * as React from 'react'
-// 🐨 you'll want the following additional things from '../pokemon':
-// fetchPokemon: the function we call to get the pokemon info
-// PokemonInfoFallback: the thing we show while we're loading the pokemon info
-// PokemonDataView: the stuff we use to display the pokemon info
 import {fetchPokemon, PokemonForm, PokemonInfoFallback, PokemonDataView} from '../pokemon'
 
 function PokemonInfo({pokemonName}) {
-  // 🐨 Have state for the pokemon (null)
   const [pokemon, setPokemon] = React.useState(null);
   const [error, setError] = React.useState(null);
-  // 🐨 use React.useEffect where the callback should be called whenever the
-  // pokemon name changes.
-  // 💰 DON'T FORGET THE DEPENDENCIES ARRAY!
-  // 💰 if the pokemonName is falsy (an empty string) then don't bother making the request (exit early).
+ 
   React.useEffect(()=> {
     if (!pokemonName){
       return;
     } else {
       setPokemon(null);
-      setError(null);
       fetchPokemon(pokemonName)
-      .then(pokemonData => setPokemon(pokemonData))
-      .catch(error => {
-        setPokemon(null);
-        setError(error);
-      })
+      .then(pokemonData => setPokemon(pokemonData), error => setError(error))
     }
   }, [pokemonName]);
 
-  // 🐨 before calling `fetchPokemon`, clear the current pokemon state by setting it to null.
-  // (This is to enable the loading state when switching between different pokemon.)
-  // 💰 Use the `fetchPokemon` function to fetch a pokemon by its name:
-  //   fetchPokemon('Pikachu').then(
-  //     pokemonData => {/* update all the state here */},
-  //   )
+  if (error) {
+    return (
+      <div role="alert">
+        There was an error: <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+      </div>
+    )
+  } else if (!pokemonName) {
+    return 'Submit a pokemon';
+  } else if(!pokemon){
+      return <PokemonInfoFallback name={pokemonName} />
 
-    if (!pokemonName) {
-      return 'Submit a pokemon';
-    } else if (error) {
-      return(
-        <div role="alert">
-          There was an error: <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
-        </div>
-      )
-    } else if(!pokemon){
-        return <PokemonInfoFallback name={pokemonName} />
-
-    } else {
-        return <PokemonDataView pokemon={pokemon} />
-    }
-
-
-
-
-
-  
-  // 🐨 return the following things based on the `pokemon` state and `pokemonName` prop:
-  //   1. no pokemonName: 'Submit a pokemon'
-  //   2. pokemonName but no pokemon: <PokemonInfoFallback name={pokemonName} />
-  //   3. pokemon: <PokemonDataView pokemon={pokemon} />
+  } else {
+      return <PokemonDataView pokemon={pokemon} />
+  }
 }
 
 function App() {
